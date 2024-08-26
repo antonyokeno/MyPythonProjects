c28e3c10308a2f2cf8ee8d8030a0a4fe62659516
antonyokeno <antonyokeno@gmail.com>
2024-08-26 05:34:58 +0300

    my first python project on udemy

---
+++ BandNameGenerator.py
@@ -0,0 +1,11 @@
+#Description
+#Create a greeting for your program.
+#Ask the user for the city that they grew up in and store it in a variable.
+#Ask the user for the name of a pet and store it in a variable.
+#Combine the name of their city and pet and show them their band name.
+
+
+print("Welcome to the Band Name Generator.")
+city =input ("What's the name of the city you grew up in?\n")
+pet = input ("What's your pet's name?\n")
+print("Your band name could be " + city + " " +pet)
